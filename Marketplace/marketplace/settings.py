25538e870fdd05b6d2b548628b95cc369b9fdc4b
Oumtata <55467453+Oumtata@users.noreply.github.com>
2021-05-17 23:59:33 -0400

    Removed info

--- Marketplace/marketplace/settings.py
+++ Marketplace/marketplace/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7^%sg_gh@p(ox(0xn5raup-yfswd+v^wqhlfp49jqvdg*a(=sy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
